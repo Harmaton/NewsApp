1e0e385b22248c8e585cebc8a3333cb8b8604e11
Harmaton <njagiiharmaton@gmail.com>
2022-06-09 12:06:33 +0300

    UI

--- app/src/main/java/com/example/newsapp/model/Article.kt
+++ app/src/main/java/com/example/newsapp/model/Article.kt
@@ -1,9 +1,11 @@
 package com.example.newsapp.model
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
+@Entity(tableName = "Article")
 data class Article(
     @PrimaryKey(autoGenerate = true)
     var id:Int?=null,
--- app/src/main/java/com/example/newsapp/repository/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsapp/repository/viewmodels/NewsViewModel.kt
@@ -10,7 +10,7 @@ import com.example.newsapp.model.NewsResponse
 import com.example.newsapp.repository.NewsRepository
 import com.example.newsapp.utils.Resource
 import kotlinx.coroutines.launch
-import okhttp3.Response
+import retrofit2.Response
 
 class NewsViewModel(val newsRepository: NewsRepository): ViewModel(){
     //for breaking news
@@ -32,17 +32,62 @@ init {
     private fun getBreakingNews(countrycode: String)= viewModelScope.launch {
       breakingNews.postValue(Resource.Loading())
         val response= newsRepository.getBreakingNews(countrycode,breakingPageNumber)
-        breakingNews.postValue(handleBreakingNewsResponse(response) )
+        breakingNews.postValue(handleBreakingNewsResponse(response))
 
     }
 
-    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse>? {
-
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
         if (response.isSuccessful){
+            response.body()?.let { resultResponse ->
+                breakingPageNumber++
+                if(breakingNewsResponse == null){
+                    breakingNewsResponse = resultResponse
+                }else {
+                    val oldArticle = breakingNewsResponse?.articles
+                    val newArticle = resultResponse.articles
+                    oldArticle?.addAll(newArticle)
+                }
+                return Resource.Success(breakingNewsResponse ?: resultResponse)
+                }
 
+            }
+        return Resource.Error(response.message())
         }
+    fun getSearchedNews(queryString: String) =viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val searchedNewsResponse = newsRepository.searchNews(queryString,searchPageNumber)
+        searchNews.postValue(handleSearchNewsResponse(searchedNewsResponse))
     }
 
+    private fun handleSearchNewsResponse(respons: Response<NewsResponse>): Resource<NewsResponse>? {
+        if (respons.isSuccessful){
+            respons.body()?.let { resultResponse ->
+                searchPageNumber++
+                if (searchNewsResponse ==null){
+                    searchNewsResponse=resultResponse
+                }else{
+                    val oldArticle=searchNewsResponse?.articles
+                    val newArticle=resultResponse.articles
+                    oldArticle?.addAll(newArticle)
+                }
+                return Resource.Success(searchNewsResponse ?: resultResponse)
+            }
+        }
+       return Resource.Error(respons.message())
+    }
+fun insertArticle(article: Article) = viewModelScope.launch {
+    newsRepository.insert(article)
+}
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.delete(article)
+    }
+    suspend fun getSavedArticles()=newsRepository.getAllArticles()
 
+    fun getBreakingNews(): LiveData<PagedList<Article>>{
+        return articles
+    }
 
 }
+
+
+
